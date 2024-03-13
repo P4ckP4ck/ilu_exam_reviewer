d01f3436fb5b8deabdedfca0a9ca3fd1149978a7
Markus de Koster <99969503+Markus-de-Koster@users.noreply.github.com>
2024-03-13 16:48:24 +0100

    Bonus point calculations following specific rules

--- bonus_main.py
+++ bonus_main.py
@@ -7,6 +7,7 @@ from utils.helper import TestReader
 
 
 if __name__ == "__main__":
+        # read in first set of bonus points (Zwischentest)
         test_results = {}
         test_files = glob.glob(os.path.join(cfg.test_path, '**/*.xlsx'), recursive=True)
         for test_file in test_files:
@@ -17,6 +18,8 @@ if __name__ == "__main__":
                 else:
                     test_results[member.id] = {'Zwischentest': member.bonus_points}
         test_df = np.floor(np.clip(pd.DataFrame(test_results).T / 3, 0, 3))
+
+        # read in second set of bonus points (Praktika)
         pra_files = glob.glob(os.path.join(cfg.pra_path, '**/*.xlsx'), recursive=True)
         pra_results = {}
         for pra_file in pra_files:
@@ -27,12 +30,27 @@ if __name__ == "__main__":
                 else:
                     pra_results[member.id] = {'Praktikum': member.bonus_points}
         pra_df = pd.DataFrame(pra_results).T
-        # add both together
-        combined_df = pd.merge(test_df, pra_df, left_index=True, right_index=True, how='outer')
-        # add together columns praktika and zwischentest
-        combined_df['Bonuspunkte'] = combined_df['Praktikum'] + combined_df['Zwischentest']
-        # clip to 5
-        combined_df = np.clip(combined_df, 0, 5).fillna(0)
-        # combined_df["Summe"] = np.clip(combined_df["Zwischentest"] + combined_df["Praktikum"], 0, 5)
 
+
+        # Additionally, old bonus points are added to the new ones
+        old_pra_bonus = pd.read_excel(os.path.join(cfg.base_path, '2023s_ETG_bonus_fixed.xlsx'))
+        old_pra_bonus.index = old_pra_bonus["Matrikelnummer"]
+        pra_df = pd.merge(pra_df, old_pra_bonus["Boni durch Praktika"], left_index=True, right_index=True, how="outer")
+        pra_df.fillna(0, inplace=True)
+        pra_df["Praktika gesamt"] = np.clip(pra_df["Boni durch Praktika"] + pra_df["Praktikum"], 0, 3)
+
+        ### Nachgeschriebene Klausurauswertung, nur gültig für dieses Semester
+        nach_klausur = pd.read_csv(os.path.join(cfg.base_path, 'Noten ETG Nachholklausur 2023-11-28.csv'))
+        # remove bonus points from students that took the exam
+        pra_df = pra_df[~pra_df.index.isin(nach_klausur['Mat.Nr.'])]
+
+        # add both together
+        combined_df = pd.merge(test_df, pra_df["Praktika gesamt"], left_index=True, right_index=True, how='outer').fillna(0)
+        # add together columns praktika and zwischentest
+        combined_df['Bonuspunkte'] = combined_df['Praktika gesamt'] + combined_df['Zwischentest']
+        # clip to 5
+        combined_df = np.clip(combined_df, 0, 5)
+        combined_df.columns = ["Boni durch Zwischentest", "Boni durch Praktika", "Summe"]
+        combined_df.index.name = "Matrikelnummer"
+        combined_df.to_excel("Bonuspunkte_WiSe_2024.xlsx")
         pass
